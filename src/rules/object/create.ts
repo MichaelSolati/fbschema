14a1e70e4e965de22de6260bcf51ad861d93f05c
Michael Solati <mkslt04@gmail.com>
2021-02-06 13:06:24 -0800

    feat(object): validate that object has all required fields and no additional ones

--- src/rules/object/create.ts
+++ src/rules/object/create.ts
@@ -9,24 +9,42 @@ export const createObject = (
   let functions: string[] = [];
   let rules: string[] = [];
 
-  for (const name in json.properties) {
-    let createdString;
-    const property = json.properties[name];
-    switch (property.type) {
-      case 'string':
-        createdString = createString(name, property);
-        functions = [...functions, ...createdString.functions];
-        rules = [...rules, ...createdString.rules];
-        break;
+  if (Object.keys(json.properties || {}).length > 0) {
+    // Create rules for each property of JSON Schema
+    for (const name in json.properties) {
+      let createdString;
+      const property = json.properties[name];
+      switch (property.type) {
+        case 'string':
+          createdString = createString(name, property);
+          functions = [...functions, ...createdString.functions];
+          rules = [...rules, ...createdString.rules];
+          break;
 
-      default:
-        break;
+        default:
+          break;
+      }
     }
+
+    // Create rule if no additional properties allowed
+    if (json.additionalProperties === false) {
+      const hasOnly = Object.keys(json.properties || {})
+        .map(key => `'${key}'`)
+        .join(', ');
+      rules.push(`data.keys().hasOnly([${hasOnly}])`);
+    }
+
+    // Create rule if there are required properties
+    if (Array.isArray(json.required) && json.required.length > 0) {
+      const hasAll = json.required.map(key => `'${key}'`).join(', ');
+      rules.push(`data.keys().hasAll([${hasAll}])`);
+    }
+
+    functions.push(`
+    function ${createFunctionName}(data) {
+      return ${rules.join(' && ') + ';'}
+    }`);
   }
-  functions.push(`
-  function ${createFunctionName}(data) {
-    return ${rules.join(' && ') + ';'}
-  }`);
 
   return {
     functions,
