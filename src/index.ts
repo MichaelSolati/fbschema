6dc8713bff14f61f67a8dd6aaca365caf3cd8eba
Michael Solati <mkslt04@gmail.com>
2025-03-27 11:05:39 -0700

    refactor(interfaces): revamp interface generation

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,21 @@
-import * as fs from 'fs';
-import * as fsExtra from 'fs-extra';
-import * as glob from 'glob';
-import {JSONSchema4} from 'json-schema';
 import * as path from 'path';
 
-import {FBSchema} from './fbschema';
-import {generateInterface} from './interfaces';
-import {generateRules, wrapRules} from './rules';
-import {getWorkingDirectory, stripString} from './utils';
+import {generateInterfaces} from './interfaces';
+import {getWorkingDirectory} from './utils';
 
-const libName = require('../package.json').name;
+const jsonSchemaFolder = require('../package.json').name;
 
 export {FBSchema} from './fbschema';
 export const generate = async (filepath = process.cwd()): Promise<void> => {
-  const collectionRules: string[] = [];
   const workingDirectory = getWorkingDirectory(filepath);
-  const schemasGlob = path.join(workingDirectory, libName, '**', '*.json');
-  const interfaceDirectory = path.join(workingDirectory, 'types', libName);
-  fsExtra.emptyDirSync(interfaceDirectory);
+  const schemasFolder = path.join(workingDirectory, jsonSchemaFolder);
+  const interfaceDirectory = path.join(
+    workingDirectory,
+    'types',
+    jsonSchemaFolder
+  );
 
-  glob(schemasGlob, {}, async (_, files) => {
-    for (const file of files) {
-      const json: FBSchema = require(file);
-      const fileName = path.basename(file).split('.').shift() as string;
-      const collectionName = stripString(json.title || fileName);
-
-      await generateInterface(
-        json as JSONSchema4,
-        collectionName,
-        fileName,
-        interfaceDirectory
-      );
-
-      collectionRules.push(generateRules(json, collectionName));
-    }
-
-    fs.writeFileSync(
-      path.join(workingDirectory, 'firestore.rules'),
-      wrapRules(collectionRules)
-    );
-  });
+  await generateInterfaces(schemasFolder, interfaceDirectory);
 };
 
 export default generate;
