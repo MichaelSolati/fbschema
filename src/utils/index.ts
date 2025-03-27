539fabe61d41415eb7b2f6a8dd0c6f70cc0822ab
Michael Solati <mkslt04@gmail.com>
2025-03-27 11:20:44 -0700

    chore(rules): remove rules code (for now)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,16 +2,3 @@ import * as path from 'path';
 
 export const getWorkingDirectory = (filepath: string): string =>
   path.isAbsolute(filepath) ? filepath : path.join(process.cwd(), filepath);
-
-export const stripString = (value: string): string =>
-  value.replace(/[^A-Z0-9]/gi, '');
-
-export const isBoolean = (x: unknown): boolean => x === true || x === false;
-
-export const isInteger = (x: unknown): boolean =>
-  isNumber(x) && Number.isInteger(x);
-
-export const isNumber = (x: unknown): boolean =>
-  typeof x === 'number' && !isNaN(Number(x));
-
-export const isString = (x: unknown): boolean => typeof x === 'string';
